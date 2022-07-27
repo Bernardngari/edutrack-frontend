1325f8f7adcbba7a0aeab355ace73e7329d7b1a7
Bernardngari <bernardngari.ke@gmail.com>
2022-07-28 00:30:38 +0300

    Style the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,9 +9,13 @@ function Home(){
 			<div  className="wrap-home">
 					<div className="side-bar">
 							<Container><div className="contact-button">Email</div></Container>
+							<hr/>
 							<Container><div className="contact-button">Tweet</div></Container>
+							<hr/>
 							<Container><div className="contact-button">WhatsApp</div></Container>
+							<hr/>
 							<Container><div className="contact-button">Direct Call</div></Container>
+							<hr/>
 					</div>
 						<div className="main-home-content">
 							<LandingPage />
