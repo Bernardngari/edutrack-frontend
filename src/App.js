e9fddf8cb3c2394ec180094524bdbe91bcff9efe
Bernardngari <bernardngari.ke@gmail.com>
2022-07-27 06:17:25 +0300

    Move state for darkmode button from Navbar  to the App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, {useState} from "react";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 function App() {
+  const[dark, setDark] = useState(false);
+  function handleDarkToggle(){
+		setDark((dark) => !dark)
+	}
   return (
     <div className="App">
-      <Navbar />
+      <Container>
+        <div className="nav-button-container">
+          <Navbar />
+          <button onClick={handleDarkToggle}>{dark? "DARK":"LIGHT"} MODE</button>
+        </div>
+      </Container>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,17 +2,15 @@ import React, {useState} from "react"
 import {NavLink} from "react-router-dom"
 
 function Navbar(){
-	function handleDarkToggle(){
-		setDark((dark) => !dark)
-	}
-	const[dark, setDark] = useState(false);
+	
+
 	return(
 		<nav>
 				<NavLink to="/">Home</NavLink>
 				<NavLink to="/about">About</NavLink>
 				<NavLink to="/tracker">Tracker</NavLink>
 				<NavLink to="/">Contact</NavLink>
-				<button onClick={handleDarkToggle}>{dark? "DARK":"LIGHT"} MODE</button>
+				
 		</nav>
 	)
 }
